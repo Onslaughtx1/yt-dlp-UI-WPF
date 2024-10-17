54a6fddd562ee22ac021b07fddd2b2816cdbe385
Michael Ferguson <mikeygsxr1k@gmail.com>
2024-10-17 17:54:23 +0200

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace yt_dlp_UI_WPF
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
